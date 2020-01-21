50af806590b5741bb558387026bd4f1b124ba760
Your Brennan Predmore <brennnann@gmail.com>
2020-01-20 23:43:51 -0600

    inject click green / click red css background property function for all times (9am-5pm)

--- script.js
+++ script.js
@@ -5,8 +5,90 @@ $(document).ready(function () {
 // 9am
 $('#saveBtn1').on('click', function () {
     $('input#9am').css('background', 'green');
+
+    $('#saveBtn1').on('click', function () {
+        $('input#9am').css('background', 'red');
+    });
+
 });
 
 // 10am
+$('#saveBtn2').on('click', function () {
+    $('input#10am').css('background', 'green');
 
+    $('#saveBtn2').on('click', function () {
+        $('input#10am').css('background', 'red');
+    });
+
+});
+
+// 11am
+$('#saveBtn3').on('click', function () {
+    $('input#11am').css('background', 'green');
+
+    $('#saveBtn3').on('click', function () {
+        $('input#11am').css('background', 'red');
+    });
+
+});
+
+// 12pm
+$('#saveBtn4').on('click', function () {
+    $('input#12pm').css('background', 'green');
+
+    $('#saveBtn4').on('click', function () {
+        $('input#12pm').css('background', 'red');
+    });
+
+});
+
+// 1pm
+$('#saveBtn5').on('click', function () {
+    $('input#1pm').css('background', 'green');
+
+    $('#saveBtn5').on('click', function () {
+        $('input#1pm').css('background', 'red');
+    });
+
+});
+
+// 2pm
+$('#saveBtn6').on('click', function () {
+    $('input#2pm').css('background', 'green');
+
+    $('#saveBtn6').on('click', function () {
+        $('input#2pm').css('background', 'red');
+    });
+
+});
+
+// 3pm
+$('#saveBtn7').on('click', function () {
+    $('input#3pm').css('background', 'green');
+
+    $('#saveBtn7').on('click', function () {
+        $('input#3pm').css('background', 'red');
+    });
+
+});
+
+// 4pm
+$('#saveBtn8').on('click', function () {
+    $('input#4pm').css('background', 'green');
+
+    $('#saveBtn8').on('click', function () {
+        $('input#4pm').css('background', 'red');
+    });
+
+});
+
+// 5pm
+$('#saveBtn9').on('click', function () {
+    $('input#5pm').css('background', 'green');
+
+    $('#saveBtn9').on('click', function () {
+        $('input#5pm').css('background', 'red');
+    });
+
+});
 
